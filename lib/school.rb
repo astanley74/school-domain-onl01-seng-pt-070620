1af291449e609f51326b669bf25dc9c6bc74fd5f
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-23 23:58:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,12 +6,12 @@ class School
     @roster = {}
   end
 
- def add_student(student_name, grad)
+ def add_student(student_name, grade)
     roster[grade] ||= []
     roster[grade] << student_name
   end
   
- def grade(student_grad)
+ def grade(student_grade)
     roster[student_grade]
   end
   
